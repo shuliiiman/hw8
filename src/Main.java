579375bc953eff89d5b07fda95e0cd3d37b50a21
Alexander Shulimov <a.shulimov@yandex.ru>
2023-08-07 13:13:08 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -7,63 +7,79 @@ public class Main {
         task4();
 
     }
-    public static void task1(){
+
+    public static void task1() {
         System.out.println("Задание номер 1");
         int[] arrays = new int[3];
-        arrays[0]=1;
-        arrays[1]=2;
-        arrays[2]=3;
+        arrays[0] = 1;
+        arrays[1] = 2;
+        arrays[2] = 3;
 
-        float[]arrays1={1.57f, 7.654f, 9.986f};
+        float[] arrays1 = {1.57f, 7.654f, 9.986f};
 
         int[] random = new int[4];
-        random[0]=435;
-        random[1]=5462;
-        random[2]=1;
-        random[3]=2483792;
+        random[0] = 435;
+        random[1] = 5462;
+        random[2] = 1;
+        random[3] = 2483792;
     }
+
     public static void task2() {
         System.out.println("Задание номер 2");
         int[] arrays = new int[3];
         arrays[0] = 1;
         arrays[1] = 2;
         arrays[2] = 3;
-
-        System.out.println(arrays[0]+", "+arrays[1]+", "+arrays[2]);
-
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i] == 3) {
+                System.out.print(arrays[i]);
+            } else System.out.print(arrays[i] + ", ");
+        }
+        System.out.println(" ");
         float[] arrays1 = {1.57f, 7.654f, 9.986f};
-
-        System.out.println(arrays1[0]+", "+arrays1[1]+", "+arrays1[2]);
+        for (int i = 0; i < arrays1.length; i++) {
+            if (arrays[i] == 3) {
+                System.out.print(arrays1[i]);
+            } else System.out.print(arrays1[i] + ", ");
+        }
+        System.out.println(" ");
     }
-    public static void task3(){
+
+    public static void task3() {
         System.out.println("Задание номер 3");
         int[] arrays = new int[3];
-        arrays[0]=1;
-        arrays[1]=2;
-        arrays[2]=3;
-
-        System.out.println(arrays[2]+", "+arrays[1]+", "+arrays[0]);
-
-        float[]arrays1={1.57f, 7.654f, 9.986f};
-
-        System.out.println(arrays1[2]+", "+arrays1[1]+", "+arrays1[0]);
+        arrays[0] = 1;
+        arrays[1] = 2;
+        arrays[2] = 3;
+        for (int i = 2; i >= 0; i--) {
+            if (arrays[i] == 1) {
+                System.out.print(arrays[i]);
+            } else System.out.print(arrays[i] + ", ");
+        }
+        System.out.println(" ");
+        float[] arrays1 = {1.57f, 7.654f, 9.986f};
+        for (int i = 2; i >= 0; i--) {
+            if (arrays1[i] == 1.57f) {
+                System.out.print(arrays1[i]);
+            } else System.out.print(arrays1[i] + ", ");
+        }
+        System.out.println(" ");
 
     }
-    public static void task4(){
+
+    public static void task4() {
         System.out.println("Задание номер 4");
         int[] arrays = new int[3];
-        arrays[0]=1;
-        arrays[1]=2;
-        arrays[2]=3;
-        for(int i =0;i<arrays.length;i=i+1){
-            if(arrays[i]%2 !=0){
-                arrays[i]+=1;
-                System.out.print(arrays[i]+", ");
+        arrays[0] = 1;
+        arrays[1] = 2;
+        arrays[2] = 3;
+        for (int i = 0; i < arrays.length; i = i + 1) {
+            if (arrays[i] % 2 != 0) {
+                arrays[i] += 1;
+                if (arrays[i] == arrays[2]) {
+                    System.out.print(arrays[i]);
+                } else System.out.print(arrays[i] + ", ");
             }
-
-
-
         }
-
     }
 }
